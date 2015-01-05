bbfab1799322b10ee98229116f25703d5b6e974f
Marcin Gosk <marcin.gosk@gmail.com>
2015-01-05 19:23:10 +0100

    Compact gateway code

--- auth-code/src/main/scala/Gateway.scala
+++ auth-code/src/main/scala/Gateway.scala
@@ -1,18 +1,18 @@
-import com.typesafe.config.Config
 import java.io.IOException
 import akka.actor.ActorSystem
 import akka.http.Http
 import akka.http.client.RequestBuilding
 import akka.http.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.model.{HttpResponse, HttpRequest}
 import akka.http.model.StatusCodes._
+import akka.http.model.{HttpRequest, HttpResponse}
 import akka.http.unmarshalling.Unmarshal
 import akka.stream.FlowMaterializer
 import akka.stream.scaladsl.{Sink, Source}
+import com.typesafe.config.Config
 import scala.concurrent.{ExecutionContext, Future}
 
-case class InternalLoginRequest(identityId: Long, authMethod: String = "codeCard")
-case class InternalReloginRequest(tokenValue: String, authMethod: String = "codeCard")
+case class InternalLoginRequest(identityId: Long, authMethod: String = "codecard")
+case class InternalReloginRequest(tokenValue: String, authMethod: String = "codecard")
 
 class Gateway(config: Config)(implicit actorSystem: ActorSystem, materializer: FlowMaterializer, ec: ExecutionContext) extends AuthCodeJsonProtocol {
   val identityManagerHost = config.getString("services.identity-manager.host")
@@ -33,7 +33,7 @@ class Gateway(config: Config)(implicit actorSystem: ActorSystem, materializer: F
       response.status match {
         case status if status.isSuccess => Unmarshal(response.entity).to[Token].map(Right(_))
         case NotFound => Future.successful(Left("Invalid token"))
-        case status => Future.failed(new IOException(s"Token request failed with status ${response.status} and error ${response.entity.toString}")) //fixme
+        case status => Future.failed(new IOException(s"Token request failed with status ${response.status} and error ${response.entity}"))
       }
     }
 
@@ -41,20 +41,16 @@ class Gateway(config: Config)(implicit actorSystem: ActorSystem, materializer: F
     requestIdentityManager(RequestBuilding.Post("/identities")).flatMap { response =>
       response.status match {
         case status if status.isSuccess => Unmarshal(response.entity).to[Identity]
-        case status => Future.failed(new IOException(s"Identity request failed with status ${response.status} and error ${response.entity.toString}")) //fixme
+        case status => Future.failed(new IOException(s"Identity request failed with status ${response.status} and error ${response.entity}"))
       }
     }
-
 
   def requestLogin(identityId: Long): Future[Token] = {
     val loginRequest = InternalLoginRequest(identityId)
     requestTokenManager(RequestBuilding.Post("/tokens", loginRequest)).flatMap { response =>
-      if (response.status.isSuccess()) {
-        Unmarshal(response.entity).to[Token]
-      } else {
-        Unmarshal(response.entity).to[String].flatMap { errorMessage =>
-          Future.failed(new IOException(s"Login request failed with status ${response.status} and error $errorMessage"))
-        }
+      response.status match {
+        case status if status.isSuccess => Unmarshal(response.entity).to[Token]
+        case status => Future.failed(new IOException(s"Login request failed with status ${response.status} and error ${response.entity}"))
       }
     }
   }
@@ -62,14 +58,10 @@ class Gateway(config: Config)(implicit actorSystem: ActorSystem, materializer: F
   def requestRelogin(tokenValue: String): Future[Option[Token]] = {
     val reloginRequest = InternalReloginRequest(tokenValue)
     requestTokenManager(RequestBuilding.Patch("/tokens", reloginRequest)).flatMap { response =>
-      if (response.status.isSuccess()) {
-        Unmarshal(response.entity).to[Token].map(Option(_))
-      } else if (response.status == NotFound) {
-        Future.successful(None)
-      } else {
-        Unmarshal(response.entity).to[String].flatMap { errorMessage =>
-          Future.failed(new IOException(s"Relogin request failed with status ${response.status} and error $errorMessage"))
-        }
+      response.status match {
+        case NotFound => Future.successful(None)
+        case status if status.isSuccess => Unmarshal(response.entity).to[Token].map(Option(_))
+        case status => Future.failed(new IOException(s"Relogin request failed with status ${response.status} and error ${response.entity}"))
       }
     }
   }
