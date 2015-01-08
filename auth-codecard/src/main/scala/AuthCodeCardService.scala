a2e07c5057f4cf601c4d91196f4287f75a8fb715
Marcin Gosk <marcin.gosk@gmail.com>
2015-01-08 10:40:42 +0100

    Move blocking clause into Repository

--- auth-codecard/src/main/scala/AuthCodeCardService.scala
+++ auth-codecard/src/main/scala/AuthCodeCardService.scala
@@ -1,35 +1,34 @@
 import java.security.SecureRandom
 import scala.concurrent.{Future, ExecutionContext}
-import scala.concurrent._
 
 class AuthCodeCardService(gateway: Gateway, repository: Repository)(implicit ec: ExecutionContext) extends AuthCodeCardConfig {
-  val random = new SecureRandom
+  private val random = new SecureRandom
 
   def register(tokenValueOption: Option[String])(implicit ec: ExecutionContext): Future[Either[String, RegisterResponse]] =
     acquireIdentity(tokenValueOption).map {
       case Right(identity) =>
         val authEntry = generateAuthEntry(identity)
         val codeCard = generateCodeCard(1, authEntry.userIdentifier)
-        blocking {repository.saveAuthEntryAndCodeCard(authEntry, codeCard)}
+        repository.saveAuthEntryAndCodeCard(authEntry, codeCard)
         Right(RegisterResponse(identity, codeCard))
       case Left(l) => Left(l)
     }
 
   def activateCode(request: ActivateCodeRequest)(implicit ec: ExecutionContext): Future[Either[String, ActivateCodeResponse]] = {
     Future.successful {
-      val codes = blocking {repository.getInactiveCodesForUser(request.userIdentifier)}
+      val codes = repository.getInactiveCodesForUser(request.userIdentifier)
       codes.length match {
         case 0 => Left("You don't have available codes")
         case _ =>
           val codeAct = codes(random.nextInt(codes.length))
-          blocking { repository.activateCode(request.userIdentifier, codeAct.cardIndex, codeAct.codeIndex) }
+          repository.activateCode(request.userIdentifier, codeAct.cardIndex, codeAct.codeIndex)
           Right(ActivateCodeResponse(codeAct.cardIndex, codeAct.codeIndex))
       }
     }
   }
 
   def login(request: LoginRequest, tokenValueOption: Option[String]): Future[Either[String, Token]] = {
-    blocking {repository.useCode(request.userIdentifier, request.cardIndex, request.codeIndex, request.code)} match {
+    repository.useCode(request.userIdentifier, request.cardIndex, request.codeIndex, request.code) match {
       case 1 =>
         tokenValueOption match {
           case None => gateway.requestLogin(repository.getIdentity(request.userIdentifier)).map(Right(_))
@@ -49,7 +48,7 @@ class AuthCodeCardService(gateway: Gateway, repository: Repository)(implicit ec:
       case Some(tokenValue) =>
         gateway.requestRelogin(tokenValue).map {
           case None => Left("Token expired or not found")
-          case Some(token) if (blocking {repository.getIdentity(request.userIdentifier)} == token.identityId) =>
+          case Some(token) if (repository.getIdentity(request.userIdentifier) == token.identityId) =>
             Right(GetCodeCardResponse(request.userIdentifier, generateCodeCard(repository.getNextCardIndex(request.userIdentifier), request.userIdentifier)))
           case Some(token) => Left("Token expired or not found")
         }
--- auth-codecard/src/main/scala/Repository.scala
+++ auth-codecard/src/main/scala/Repository.scala
@@ -1,6 +1,6 @@
-import com.typesafe.config.Config
 import scala.slick.lifted.{ProvenShape, Tag}
 import scala.slick.driver.PostgresDriver.simple._
+import scala.concurrent._
 
 case class AuthEntry(userIdentifier: String, identityId: Long, createdAt: Long, lastCard: Long)
 
@@ -25,41 +25,54 @@ class Codes(tag: Tag) extends Table[Code](tag, "code") {
   override def * : ProvenShape[Code] = (userIdentifier, cardIndex, codeIndex, code, createdAt, activatedAt, usedAt) <> (Code.tupled, Code.unapply)
 }
 
-class Repository extends AuthCodeCardConfig{
+class Repository extends AuthCodeCardConfig {
   val db = Database.forURL(url = dbUrl, user = dbUser, password = dbPassword, driver = "org.postgresql.Driver")
   val codesQuery = TableQuery[Codes]
   val authEntriesQuery = TableQuery[AuthEntries]
 
-  def useCode(userIdentifier: String, cardIdx: Long, codeIdx: Long, code: String): Int =
+  def useCode(userIdentifier: String, cardIdx: Long, codeIdx: Long, code: String): Int = blocking {
     db.withSession { implicit s =>
       codesQuery.filter(codeQ => (codeQ.userIdentifier === userIdentifier && codeQ.cardIndex === cardIdx && codeQ.codeIndex === codeIdx && codeQ.code === code && codeQ.usedAt.isEmpty === true && codeQ.activatedAt >= (System.currentTimeMillis - codeActiveTime)))
         .map(_.usedAt).update(Some(System.currentTimeMillis))
     }
-
-  def getIdentity(userIdentifier: String) = db.withSession { implicit s =>
-    authEntriesQuery.filter(line => (line.userIdentifier === userIdentifier)).map(_.identityId).first
   }
 
-  def getNextCardIndex(userIdentifier: String): Long = db.withSession { implicit s => s.withTransaction {
-    val next = authEntriesQuery.filter(line => (line.userIdentifier === userIdentifier)).map(_.lastCard).first + 1
-    authEntriesQuery.filter(line => (line.userIdentifier === userIdentifier)).map(_.lastCard).update(next)
-    next
-  }
-  }
-
-  def saveAuthEntryAndCodeCard(authEntry: AuthEntry, codeCard: CodeCard) = db.withSession { implicit s => s.withTransaction {
-    authEntriesQuery += authEntry
-    codeCard.codes.zipWithIndex.map { case (code, idx) =>
-      codesQuery += Code(codeCard.userIdentifier, codeCard.id, idx.toLong, code, System.currentTimeMillis())
+  def getIdentity(userIdentifier: String) = blocking {
+    db.withSession { implicit s =>
+      authEntriesQuery.filter(line => (line.userIdentifier === userIdentifier)).map(_.identityId).first
     }
   }
+
+  def getNextCardIndex(userIdentifier: String): Long = blocking {
+    db.withSession { implicit s =>
+      s.withTransaction {
+        val next = authEntriesQuery.filter(line => (line.userIdentifier === userIdentifier)).map(_.lastCard).first + 1
+        authEntriesQuery.filter(line => (line.userIdentifier === userIdentifier)).map(_.lastCard).update(next)
+        next
+      }
+    }
   }
 
-  def getInactiveCodesForUser(userIdentifier: String): Seq[Code] = db.withSession { implicit s =>
-    codesQuery.filter(code => (code.userIdentifier === userIdentifier && code.activatedAt.isEmpty === true)).list
+  def saveAuthEntryAndCodeCard(authEntry: AuthEntry, codeCard: CodeCard) = blocking {
+    db.withSession { implicit s =>
+      s.withTransaction {
+        authEntriesQuery += authEntry
+        codeCard.codes.zipWithIndex.map { case (code, idx) =>
+          codesQuery += Code(codeCard.userIdentifier, codeCard.id, idx.toLong, code, System.currentTimeMillis())
+        }
+      }
+    }
   }
 
-  def activateCode(userIdentifier: String, cardIndex: Long, codeIndex: Long) = db.withSession { implicit s =>
-    codesQuery.filter(code => (code.userIdentifier === userIdentifier && code.cardIndex === cardIndex && code.codeIndex === codeIndex)).map(_.activatedAt).update(Some(System.currentTimeMillis))
+  def getInactiveCodesForUser(userIdentifier: String): Seq[Code] = blocking {
+    db.withSession { implicit s =>
+      codesQuery.filter(code => (code.userIdentifier === userIdentifier && code.activatedAt.isEmpty === true)).list
+    }
+  }
+
+  def activateCode(userIdentifier: String, cardIndex: Long, codeIndex: Long) = blocking {
+    db.withSession { implicit s =>
+      codesQuery.filter(code => (code.userIdentifier === userIdentifier && code.cardIndex === cardIndex && code.codeIndex === codeIndex)).map(_.activatedAt).update(Some(System.currentTimeMillis))
+    }
   }
 }
